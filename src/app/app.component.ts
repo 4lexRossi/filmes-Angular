736d8cdb3db3d896a7a11cd95db48bfac20a530c
4lexRossi <devalexrossi@gmail.com>
2020-10-11 20:51:43 -0300

    change directive and component selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'dio-root',
+  selector: 'fpt-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
